b691452db8de3d5ddb4c7a497c9aba915b269c50
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-02-22 05:14:13 +0000

    far as I got

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,29 @@
 class PlacesController < ApplicationController
     def index
         @places = Place.all
-    end
-
-    def new
+      end
+      def show
+        @place = Place.find(params["id"])
+      end
+      def new
         @place = Place.new
-    end
-
-    def create
+      end
+      def create
         @place = Place.new(params["place"])
         @place.save
         redirect_to "/places"
-    end
-
-    def show
+      end
+      def edit
         @place = Place.find(params["id"])
-        @post = Post.where({id: "#{@place.id}"})
-    end
+      end
+      def update
+        @place = Place.find(params["id"])
+        @place.update(params["place"])
+        redirect_to "/places"
+      end
+      def destroy
+        @place = Place.find(params["id"])
+        @place.destroy
+        redirect_to "/places"
+      end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,17 @@ class PostsController < ApplicationController
     def index
     @posts = Post.all
   end
-
+  def show
+    @post = Post.find(params["id"])
+  end
   def new
     @post = Post.new
-    @place = Place.find(parans["place_id"])
+    @place = Place.find(params["place_id"])
     @post.place_id = @place.id
   end
-
   def create
-    @post = Post.new(parans["post"])
+    @post = Post.new(params["post"])
     @post.save
-    redirect_to "/places/#{@post.place_id}"
+    redirect_to "/places/#{@post.place.id}"
   end
-
-  def show
-    @post = Post.find(parans["id"])
   end
-end
